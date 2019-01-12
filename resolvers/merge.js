25a4c3002b0af8ac2938d669dab9fcbd41c76963
wiktor Płocki <wiktor.plocki@gmail.com>
2019-01-12 11:08:38 +0100

    Cleanup merge.js

--- resolvers/merge.js
+++ resolvers/merge.js
@@ -1,5 +1,4 @@
 const Technology = require('../models/technology');
-const Project = require('../models/project');
 
 const technologies = async techIds => {
   try {
@@ -12,52 +11,6 @@ const technologies = async techIds => {
     });
   } catch (err) {
     throw err;
-  }
-};
-
-const singleTechnology = async techId => {
-  try {
-    const technology = await Technology.findById(techId);
-    return {
-      ...technology._doc,
-      _id: technology.id
-    };
-  } catch (err) {
-    throw err;
-  }
-};
-
-const projects = async () => {
-  try {
-    const projects = await Project.find();
-    return projects.map(project => transformProjects(project));
-  } catch (err) {
-    throw err;
-  }
-};
-
-const singleProject = async projectId => {
-  try {
-    const project = await Project.findById(projectId);
-    return transformProjects(project);
-  } catch (err) {
-    throw err;
-  }
-};
-
-const addTechnologiesToProject = async (projectId, technologies) => {
-  try {
-    const foundProject = await Project.findById(projectId);
-    await technologies.forEach(label => {
-      const foundTech = Technology.findOne({ label });
-      if (!foundTech) {
-        throw new Error('Technology specified not found!');
-      }
-      return foundProject.technologies.push(foundTech);
-    });
-    await foundProject.save();
-  } catch (err) {
-    throw new Error(err);
   }
 };
 
