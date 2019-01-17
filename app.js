fa0b195276a13dab0064f77a4273bee45b226698
wiktor Płocki <wiktor.plocki@gmail.com>
2019-01-17 11:27:40 +0100

    Add CORS handling, separate app starting and DB connection

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const mongoose = require('mongoose');
 const gqlSchema = require('./schemas');
 const gqlResolver = require('./resolvers');
 const isAuth = require('./middleware/auth');
+const cors = require('./middleware/cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(isAuth);
 app.use(
   '/api',
@@ -21,18 +23,18 @@ app.use(
 
 mongoose
   .connect(
-    `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}${
+    `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${
       process.env.MONGO_URL
     }`,
     { useNewUrlParser: true }
   )
-  .then(() =>
-    app.listen(process.env.PORT, () =>
-      console.log(`Server listening on port ${process.env.PORT}`)
-    )
-  )
+  .then(() => console.log('Connected to DB'))
   .catch(err => {
     throw new Error(err);
   });
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running at port ${process.env.PORT}`)
+);
 
 module.exports = app;
